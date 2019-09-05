90520fe271acad859ba4dc8e1d6a38aa848dcfc5
Lucas Andión Montáns <andion@gmail.com>
2019-09-05 14:51:07 +0200

    Fix when a Person object has no link. Fix typo on email

--- src/person.js
+++ src/person.js
@@ -3,7 +3,7 @@ import Link from './link';
 export default class Person {
   constructor(object) {
     this.name = object.name;
-    this.email = object.emil;
-    this.link = new Link(object.link);
+    this.email = object.email;
+    this.link = object.link && new Link(object.link);
   }
 }
